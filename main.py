1a51a56707d41cc10a3b2b78f61b76cd208d53ce
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-10-22 00:27:50 +0330

    add archive def for save response

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import requests
+import json
+
 from config import url
 
 
 def get_rates():
     response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        return None
+
+
+def archive(filename, rates):
+    with open(f'Archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
 
 
 if __name__ == "__main__":
-    get_rates()
+    res = get_rates()
+    archive(res["timestamp"], res["rates"])
