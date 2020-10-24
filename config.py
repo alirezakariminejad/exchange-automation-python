795f69bdef09b628333b8aec4eabd0a8069ffc29
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-10-24 22:38:00 +0330

    add new rules

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ BASE_PATH = "http://data.fixer.io/api/latest?access_key="
 API_KEY = "74186fd800b83056bf66acc1901a7218"
 
 url = BASE_PATH + API_KEY
+
+receive_mail = "alireza.kariminejad@yahoo.com"
+
+rules = {
+    "archive": True,
+    "send_mail": True,
+    "preferred_rates": ["AED", "IRR", "USD", "BTN"]
+}
