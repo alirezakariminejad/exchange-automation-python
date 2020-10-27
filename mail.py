e8142db1db4c4e7e97e47526a3208a7af50498c0
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-10-27 20:04:29 +0330

    upadate code

--- mail.py
+++ mail.py
@@ -1,12 +1,12 @@
 import smtplib
-from config import receiver_mail
+from config import rules
 from email.mime.text import MIMEText
 
 
 def send_msg_smtp(subject, body):
     msg = MIMEText(body)
     msg["subject"] = subject
-    msg["to"] = receiver_mail
+    msg["to"] = rules["mail"]["receiver_mail"]
     msg["from"] = "design@niafam.com"
     with smtplib.SMTP("smtp.mailtrap.io", 2525) as mail_server:
         mail_server.login("519fafc3271284", "a40a4f63e858d5")
